81e91bbde6ad389eea4281475f4688c5d5ce12c7
HastiLong <hastilong@gmail.com>
2025-11-17 22:22:47 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,5 +14,6 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/hasti-long-dwellings/", // correct base for GitHub Pages subpath
+  base: "/hasti-long-dwellings/", // correct base for GitHub Pages subpath,
+    plugins: [react()],
 }));
