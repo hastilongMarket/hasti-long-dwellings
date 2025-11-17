2243226b5edead6768cc746689e823f92972b6ed
HastiLong <hastilong@gmail.com>
2025-11-17 22:01:25 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom"; // <-- import HashRouter
+import { HashRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <HashRouter basename="/hasti-long-dwellings">
+  <HashRouter>
     <App />
   </HashRouter>
 );
